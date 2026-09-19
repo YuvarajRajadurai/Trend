fe29986453b3854c94c4efd4b20ec0d0aeabff08
YuvarajRajadurai <jryuvaraj@gmail.com>
2026-09-19 20:53:24 +0530

    update ami on main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,17 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd*/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
